29ad23075c17328bac321034ecd43e61c15ddc29
FabricioCruzCBA <fabriciogmcruz@hotmail.com>
2024-09-30 17:23:02 -0400

    Ta dificil

--- routes/web.php
+++ routes/web.php
@@ -116,7 +116,7 @@ route::get('/divida/edit/{id}', [DividaController::class, 'showEdit']);
 route::post('/divida/edit', [DividaController::class, 'atualizarRegistro']);
 route::get('/divida/delete/{id}', [DividaController::class, 'showDelete']);
 route::post('divida/delete', [DividaController::class, 'delete']);
-route::post('/divida/pqp', function(Request $request){
+route::post('/divida/pqp/', function(Request $request){
     echo($requet->id);
 });
 
