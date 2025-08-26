9032bdd7b75343cfd418d69813c207f6252fe957
FabricioCruzCBA <fabriciogmcruz@hotmail.com>
2025-08-26 14:05:50 -0400

    ajuste do model usuario

--- app/Models/usuario.php
+++ app/Models/usuario.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable; // <-- obrigatório
-use NotificationChannels\WebPush\HasPushSubscriptions;
+
 
 class usuario extends Model
 {
