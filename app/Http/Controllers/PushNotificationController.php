7f8f72a0b3c44e9e6d7e52e50798bbdc20fc6855
FabricioCruzCBA <fabriciogmcruz@hotmail.com>
2025-09-03 19:10:05 -0400

    notificação funcionando finalizada

--- app/Http/Controllers/PushNotificationController.php
+++ app/Http/Controllers/PushNotificationController.php
@@ -33,48 +33,54 @@ class PushNotificationController extends Controller
 
         $webPush = new WebPush($auth);
 
-        
-        $payload = json_encode([
-            "endpoint" => "https://fcm.googleapis.com/fcm/send/dy60voc6eW4:APA91bGzbJIP9wEsJMtXGjn3963Owic0QQPQHpfrz5fZBfXNGpxmcZxca_FWfR9suVP3nLOYUCSTSu1NhHv_i4uCFkxSMC9o8Tus35YJo_UdYfyW9-URPyaky_YKivsJ2pqiAEgAvLwK",
-                    "keys" => [
-                        'p256dh' => 'BBdZ7fJwk8q_sqb42C8AaqO1bSXuqDfa3bA5lGg7_KTFUTh7a0PJly-q1MvlThzn8jkTN--hariKG3rEvi9IIg0',
-                        'auth' => 'QzI2qXuW9wh9Wtk5GiUuBw'
-                    ],
-        ]);
 
         $assinaturas = PushNotification::all();
-
         
-        $subscription = Subscription::create(json_decode($payload, true));
-
-        // array of notifications
-        $notifications = [
-            [
-                'subscription' => $subscription,
-                'payload' => '{"title":"Notificação teste!", "body":"Essa é uma notificação teste"}',
-            ],
-        ];
-
         
-        // send multiple notifications with payload
-        foreach ($notifications as $notification) {
-            $webPush->queueNotification(
-                $notification['subscription'],
-                $notification['payload'] // optional (defaults null)
-            );
-        }
+        foreach($assinaturas as $assinatura){
+            $valor = json_decode($assinatura->subscriptions, true);
+            echo($valor['endpoint']);
+            echo('<br>');
+            echo($valor['keys']['p256dh']);
+            //dd($valor);
+            $asing = json_encode([
+                "endpoint" => $valor['endpoint'],
+                "keys" => [
+                    'p256dh' => $valor['keys']['p256dh'], 
+                    'auth' => $valor['keys']['auth']
+                ]
+            ]);
 
-        /**
-         * Check sent results
-         * @var MessageSentReport $report
-         */
-        foreach ($webPush->flush() as $report) {
-            $endpoint = $report->getRequest()->getUri()->__toString();
+            $subscription = Subscription::create(json_decode($asing, true));   
 
-            if ($report->isSuccess()) {
-                echo "[v] Message sent successfully for subscription {$endpoint}.";
-            } else {
-                echo "[x] Message failed to sent for subscription {$endpoint}: {$report->getReason()}";
+            // array of notifications
+            $notifications = [
+                [
+                    'subscription' => $subscription,
+                    'payload' => '{"title":"Notificação teste!", "body":"Essa é uma notificação teste"}',
+                ],
+            ];
+
+            // send multiple notifications with payload
+            foreach ($notifications as $notification) {
+                $webPush->queueNotification(
+                    $notification['subscription'],
+                    $notification['payload'] // optional (defaults null)
+                );
+            }
+
+            /**
+             * Check sent results
+             * @var MessageSentReport $report
+             */
+            foreach ($webPush->flush() as $report) {
+                $endpoint = $report->getRequest()->getUri()->__toString();
+
+                if ($report->isSuccess()) {
+                    echo "[v] Message sent successfully for subscription {$endpoint}.";
+                } else {
+                    echo "[x] Message failed to sent for subscription {$endpoint}: {$report->getReason()}";
+                }
             }
         }
     }
